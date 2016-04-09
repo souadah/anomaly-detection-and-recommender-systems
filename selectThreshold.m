30d38fbca958e06b76532578ac5dc1ca0d3fb892
Sarah <souadah@gmail.com>
2016-04-09 08:27:22 -0400

    Find best threshold for anomalies

--- selectThreshold.m
+++ selectThreshold.m
@@ -22,19 +22,18 @@ for epsilon = min(pval):stepsize:max(pval)
     %               
     % Note: You can use predictions = (pval < epsilon) to get a binary vector
     %       of 0's and 1's of the outlier predictions
-
-
-
-
-
-
-
-
-
-
-
-
-
+    
+    predictions = (pval < epsilon);
+    
+    tp = sum(predictions(yval == 1));
+    fp = sum(predictions(yval == 0));
+    fn = sum(yval(predictions == 0));
+    
+    prec = tp / (tp + fp);
+    rec = tp / (tp + fn);
+    
+    F1 = (2 * prec * rec) / (prec + rec);
+    
     % =============================================================
 
     if F1 > bestF1
